94a554b27f858e0de89653511fd41e5b68449f9e
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-02-16 17:10:24 +0300

    unitalk.py

--- scripts/unitalk.py
+++ scripts/unitalk.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+#from seleniumwire import webdriver
+from fake_useragent import UserAgent
 from flask import Flask, jsonify
 from twocaptcha import TwoCaptcha
 from selenium import webdriver
@@ -18,8 +20,34 @@ url = 'https://my.unitalk.cloud/enter.html#auth'
 api_key = '7f728c25edca4f4d0e14512d756d6868'
 site_key = '6LdTrQYeAAAAAB5wwjOovTVSrXDmPB7we-9dYi0o'
 
-# driver = webdriver.Chrome()
-# driver.maximize_window()
+#PROXY_CONSTANS
+
+proxy_address = "45.130.254.133"
+proxy_login = 'K0nENe'
+proxy_password = 'uw7RQ3'
+proxy_port = 8000
+
+proxy_options = {
+    "proxy":{
+        "http":f"http://{proxy_login}:{proxy_password}@45.130.254.133:8000",
+        "https": f"http://{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}"
+    }
+}
+
+#CHROME OPTIONS
+
+options = webdriver.ChromeOptions()
+user_agent = UserAgent()
+options.headless = False
+options.add_argument(f"user-agent={user_agent.random}")
+options.add_argument("--disable-save-password-bubble")
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_argument("--disable-extensions")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-browser-side-navigation")
+options.add_argument("--disable-gpu")
+#options.add_argument("--headless")
+options.add_experimental_option("detach", True)
 
 def solve_captcha(sitekey: str, url: str) -> str:
     solver = TwoCaptcha(api_key)
@@ -41,6 +69,7 @@ def captcha_and_login(driver):
         input_password.send_keys(user_password)
     except Exception as e:
         print(f"INPUT ERROR \n{e}")
+        return None
 
     try:
         driver.implicitly_wait(30)
@@ -57,19 +86,21 @@ def captcha_and_login(driver):
         driver.execute_script("arguments[0].click();", button_login)
     except Exception as e:
         print(f"BUTTON ERROR \n{e}")
+        return None
     sleep(5)
 
 def get_wallet():
-    with webdriver.Chrome() as driver:
+    with webdriver.Chrome(options=options) as driver:
         captcha_and_login(driver)
         driver.get('https://my.unitalk.cloud/index.html#balance')
 
         try:
-            driver.implicitly_wait(10)
+            driver.implicitly_wait(30)
             input_balance = driver.find_element(By.XPATH, '//*[@id="content"]/div/div[1]/div[1]/div[2]/div[2]/div/div/label/input')
             input_balance.send_keys('3')
         except Exception as e:
             print(f"INPUT BALANCE ERROR \n{e}")
+            return None
 
         try:
             driver.implicitly_wait(10)
@@ -77,6 +108,7 @@ def get_wallet():
             driver.execute_script("arguments[0].click();", add_value)
         except Exception as e:
             print(f"ADD VALUE BUTTON \n{e}")
+            return None
 
         try:
             driver.implicitly_wait(10)
@@ -92,6 +124,7 @@ def get_wallet():
             driver.execute_script("arguments[0].click();", buy_)
         except Exception as e:
             print(f"BUY BUTTON ERROR \n{e}")
+            return None
 
         try:
             driver.implicitly_wait(10)
@@ -103,6 +136,7 @@ def get_wallet():
             driver.execute_script("arguments[0].click();", next_step)
         except Exception as e:
             print(f"TETHER TRC20 BUTTON ERROR \n{e}")
+            return None
 
         amount = WebDriverWait(driver, 30).until(
             EC.visibility_of_element_located(
