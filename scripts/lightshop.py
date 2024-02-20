da578ee773cbdc6a6baafa9f5bf653b82cfbe5a5
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-02-20 15:55:55 +0300

    lightshop.py

--- scripts/lightshop.py
+++ scripts/lightshop.py
@@ -2,9 +2,8 @@ import time
 import pyautogui
 from flask import jsonify
 from fake_useragent import UserAgent
-from selenium_driverless import webdriver
+from selenium_driverless.sync import webdriver
 from selenium_driverless.types.by import By
-import asyncio
 
 #CONSTANS
 
@@ -18,22 +17,22 @@ options = webdriver.ChromeOptions()
 user_agent = UserAgent()
 options.add_argument(f"user-agent={user_agent.random}")
 
-async def get_wallet():
-    async with webdriver.Chrome(options=options) as driver:
-        await driver.get(url)
-        await driver.refresh()
-        await driver.maximize_window()
+def get_wallet():
+     with webdriver.Chrome(options=options) as driver:
+        driver.get(url)
+        driver.refresh()
+        driver.maximize_window()
 
         try:
-            choose_product = await driver.find_element(By.CSS_SELECTOR, 'div.table-responsive > table > tbody > tr:nth-child(13) > td.text-center > a > i', timeout=20)
-            await driver.execute_script("arguments[0].click();", choose_product)
+            choose_product = driver.find_element(By.CSS_SELECTOR, 'div.table-responsive > table > tbody > tr:nth-child(13) > td.text-center > a > i', timeout=20)
+            driver.execute_script("arguments[0].click();", choose_product)
         except Exception as e:
             print(f'CHOOSE PRODUCT ERROR \n{e}')
 
         try:
-            input_email_in_product = await driver.find_element(By.CSS_SELECTOR, '#order > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=text]', timeout=20)
+            input_email_in_product = driver.find_element(By.CSS_SELECTOR, '#order > table > tbody > tr:nth-child(2) > td:nth-child(2) > input[type=text]', timeout=20)
             time.sleep(2)
-            await input_email_in_product.write(user_login)
+            input_email_in_product.write(user_login)
         except Exception as e:
             print(f"INPUT EMAIL ERROR \n{e}")
 
@@ -46,35 +45,36 @@ async def get_wallet():
         pyautogui.click()
 
         try:
-            start_buy = await driver.find_element(By.CSS_SELECTOR, '#order > footer > button', timeout=40)
+            start_buy = driver.find_element(By.CSS_SELECTOR, '#order > footer > button', timeout=40)
             time.sleep(2)
-            await driver.execute_script("arguments[0].click();", start_buy)
+            driver.execute_script("arguments[0].click();", start_buy)
         except Exception as e:
             print(f"START BUY BUTTON ERROR \n{e}")
             return "Not work"
 
         try:
-            choose_tether = await driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form[2]/div[13]', timeout=40)
+            choose_tether = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/form[2]/div[13]', timeout=40)
             time.sleep(3)
-            await choose_tether.click()
+            choose_tether.click()
         except Exception as e:
             print(f"CHOOSE TETHER ERROR \n{e}")
             return "Not work"
 
         try:
-            accept = await driver.find_element(By.CSS_SELECTOR, 'div.payment-container > div.popup__bg.active > div > div > input', timeout=20)
+            accept = driver.find_element(By.CSS_SELECTOR, 'div.payment-container > div.popup__bg.active > div > div > input', timeout=30)
             time.sleep(5)
-            await driver.execute_script("arguments[0].click();", accept)
+            driver.execute_script("arguments[0].click();", accept)
         except Exception as e:
             print(f"CONTINUE BUTTON ERROR \n{e}")
             return "Not work"
 
-        try:
-            amount_element = await driver.find_element(By.ID, 'pay_amount', timeout=10)
-            amount = await amount_element.text
 
-            address_element = await driver.find_element(By.ID, 'crypto_address', timeout=10)
-            address = await address_element.text
+        try:
+            amount_element = driver.find_element(By.ID, 'pay_amount', timeout=10)
+            amount = amount_element.text
+
+            address_element = driver.find_element(By.ID, 'crypto_address', timeout=10)
+            address = address_element.text
         except Exception as e:
             print(f"DATA ERROR \n{e}")
             return "Not work"
@@ -86,6 +86,5 @@ async def get_wallet():
         }
 
 def wallet():
-    wallet_data = asyncio.run(get_wallet())
-    #print(wallet_data)
+    wallet_data = get_wallet()
     return jsonify(wallet_data)
