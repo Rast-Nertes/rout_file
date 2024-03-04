9067acf23048254aeb738dfabeb4802ea49b13d7
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-03-04 10:18:50 +0300

    Add files via upload

---
+++ scripts/ewallets.py
@@ -0,0 +1,114 @@
+from flask import jsonify
+from selenium import webdriver
+from time import sleep
+from fake_useragent import UserAgent
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+#CONSTANS
+
+url = 'https://ewallets.cc/product/oldubil/'
+user_login = "rwork875"
+user_password = "TQOAyI%Kj078"
+
+#CHROME CONSTANS
+
+options = webdriver.ChromeOptions()
+user_agent = UserAgent()
+options.add_argument(f"user-agent={user_agent.random}")
+options.add_argument("--disable-save-password-bubble")
+options.headless = False
+
+
+def get_wallet():
+    with webdriver.Chrome(options=options) as driver:
+        actions = ActionChains(driver)
+        driver.maximize_window()
+        driver.get('https://ewallets.cc/my-account/')
+
+        try:
+            driver.implicitly_wait(10)
+            input_email = driver.find_element(By.ID, 'username')
+            input_email.clear()
+            input_email.send_keys(user_login)
+
+            driver.implicitly_wait(10)
+            input_password = driver.find_element(By.ID, 'password')
+            input_password.clear()
+            input_password.send_keys(user_password)
+        except Exception as e:
+            print(f"INPUT DATA ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(10)
+            button_login = driver.find_element(By.CSS_SELECTOR, 'div.page-wrapper.my-account.mb > div > div > div.account-container.lightbox-inner > div > form > p:nth-child(3) > button')
+            sleep(1.5)
+            driver.execute_script("arguments[0].click();", button_login)
+        except Exception as e:
+            print(f"BUTTON LOGIN ERROR \n{e}")
+        #input("Press")
+
+        #driver.get(url)
+
+        # try:
+        #     driver.implicitly_wait(10)
+        #     add_to_busket_button = driver.find_element(By.CSS_SELECTOR, 'div > div.product-info.summary.col-fit.col.entry-summary.product-summary.form-minimal > form > button')
+        #     sleep(1.5)
+        #     driver.execute_script("arguments[0].click();", add_to_busket_button)
+        # except Exception as e:
+        #     print(f"ADD TO BUSKET ERROR \n{e}")
+
+        #input("Press")
+
+        driver.get('https://ewallets.cc/checkout/')
+
+        try:
+            driver.implicitly_wait(10)
+            input_email = driver.find_element(By.ID, 'billing_email')
+            input_email.clear()
+            sleep(2)
+            input_email.send_keys('rwork875@gmail.com')
+        except Exception as e:
+            print(f"INPUT EMAIL ERROR \n{e}")
+        #
+        # try:
+        #     driver.implicitly_wait(10)
+        #     choose_crypto_currency = driver.find_element(By.ID, 'select2-payment_cryptapi_coin-container')
+        #     sleep(1.5)
+        #     choose_crypto_currency.click()
+        #     sleep(1)
+        # except Exception as e:
+        #     print(f"CHOOSE CRYPTO ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(10)
+            place_order = driver.find_element(By.ID, 'place_order')
+            sleep(1.5)
+            driver.execute_script("arguments[0].click();", place_order)
+        except Exception as e:
+            print(f"PLACE ORDER BUTTON ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(20)
+            amount = driver.find_element(By.CSS_SELECTOR, 'div.ca_payments_wrapper > div.ca_details_box > div:nth-child(1) > button > span:nth-child(1) > b').text
+
+            driver.implicitly_wait(20)
+            address = driver.find_element(By.CSS_SELECTOR, 'div.ca_payments_wrapper > div.ca_details_box > div:nth-child(6) > button > span:nth-child(1)').text
+
+            return {
+                "address": address,
+                "amount": amount,
+                "currency": "usdt"
+            }
+        except Exception as e:
+            print(f"DATA ERROR \n{e}")
+
+
+def wallet():
+    wallet_data = get_wallet()
+    print(wallet_data)
+    return jsonify(wallet_data)
---
+++ scripts/usasmmit.py
@@ -0,0 +1,135 @@
+from flask import jsonify
+from selenium import webdriver
+from time import sleep
+from fake_useragent import UserAgent
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+#CONSTANS
+
+url = 'https://usasmmit.com'
+user_login = "kiracase34@gmail.com"
+user_password = "kiramira555"
+
+#PROXY_CONSTANS
+
+proxy_address = "45.130.254.133"
+proxy_login = 'K0nENe'
+proxy_password = 'uw7RQ3'
+proxy_port = 8000
+
+proxy_options = {
+    "proxy":{
+        "http":f"http://{proxy_login}:{proxy_password}@45.130.254.133:8000",
+        "https": f"http://{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}"
+    }
+}
+
+#CHROME CONSTANS
+
+options = webdriver.ChromeOptions()
+user_agent = UserAgent()
+options.add_argument(f"user-agent={user_agent.random}")
+options.add_argument("--disable-save-password-bubble")
+options.headless = False
+
+
+def get_wallet():
+    with webdriver.Chrome(options=options) as driver:
+        actions = ActionChains(driver)
+        driver.get(url)
+        driver.maximize_window()
+
+        try:
+            driver.implicitly_wait(10)
+            purchase_button = driver.find_element(By.CSS_SELECTOR, '#prctbl9 > a')
+            sleep(1.5)
+            driver.execute_script("arguments[0].click();", purchase_button)
+        except Exception as e:
+            print(f"PURCHASE BUTTON ERROR \n{e}")
+
+        try:
+            # driver.implicitly_wait(10)
+            # input_count = driver.find_element(By.CSS_SELECTOR, '#quantity_65e02fff83238')
+            # sleep(1.5)
+            # input_count.clear()
+            # input_count.send_keys("1")
+            #
+            # driver.implicitly_wait(10)
+            # update_cart = driver.find_element(By.CSS_SELECTOR, 'div.rh-container.clearfix.mt30.mb30 > div > article > div > form > table > tbody > tr:nth-child(2) > td > button')
+            # sleep(1.5)
+            # driver.execute_script("arguments[0].click();", update_cart)
+
+            driver.implicitly_wait(10)
+            proceed_to_checkout = driver.find_element(By.CSS_SELECTOR, 'div.rh-container.clearfix.mt30.mb30 > div > article > div > div.cart-collaterals > div > div > a')
+            sleep(1.5)
+            driver.execute_script("arguments[0].click();", proceed_to_checkout)
+        except Exception as e:
+            print(f"INPUT ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(10)
+            input_first_name = driver.find_element(By.ID, 'billing_first_name')
+            input_first_name.clear()
+            input_first_name.send_keys("Kira")
+
+            driver.implicitly_wait(10)
+            input_last_name = driver.find_element(By.ID, 'billing_last_name')
+            input_last_name.clear()
+            input_last_name.send_keys("Ivanova")
+
+            driver.implicitly_wait(10)
+            input_email_address = driver.find_element(By.ID, 'billing_email')
+            input_email_address.clear()
+            input_email_address.send_keys(user_login)
+        except Exception as e:
+            print(f"INPUT DATA ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(10)
+            ticket = driver.find_element(By.ID, 'terms')
+            sleep(1.5)
+            driver.execute_script("arguments[0].click();", ticket)
+        except Exception as e:
+            print(f"TICKET ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(10)
+            choose_currency = driver.find_element(By.ID, 'CsaltCoin')
+            sleep(2)
+            choose_currency.click()
+
+            for _ in range(3):
+                actions.send_keys(Keys.ARROW_DOWN).perform()
+                sleep(0.5)
+
+            actions.send_keys(Keys.ENTER).perform()
+            sleep(1)
+        except Exception as e:
+            print(f"CHOOSE CURRENCY ERROR \n{e}")
+
+        try:
+            driver.implicitly_wait(30)
+            address = driver.find_element(By.ID, 'alt-coinAddress').get_attribute("value")
+
+            driver.implicitly_wait(30)
+            amount = driver.find_element(By.CSS_SELECTOR, '#wapg_order_review > div > div > h3 > strong').text
+
+            return {
+                "address": address,
+                "amount": amount,
+                "currency": "usdt"
+            }
+
+        except Exception as e:
+            print(f"DATA ERROR \n{e}")
+            return None
+
+
+def wallet():
+    wallet_data = get_wallet()
+    print(wallet_data)
+    return jsonify(wallet_data)
