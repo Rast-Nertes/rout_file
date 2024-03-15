cc67c5a449a39eeca2bfab917df99e4864c40481
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-03-15 16:56:53 +0300

    Update safeassets.py

--- scripts/safeassets.py
+++ scripts/safeassets.py
@@ -32,30 +32,37 @@ proxy_ip = "196.19.121.187"
 async def login(driver):
     await driver.set_single_proxy(f"http://{proxy_name}:{proxy_pass}@{proxy_ip}:{proxy_port}")
     sleep(1)
-    await driver.get(url)
-    sleep(3)
-    await driver.get(url)
+    await driver.get('https://www.safeassets.com/login/say/invalid_login/e/username')
     await driver.maximize_window()
-
+    
     try:
-        button_login = await driver.find_element(By.CSS_SELECTOR, 'div > div.header__options > a.header__sing-in', timeout=60)
-        await driver.execute_script("arguments[0].click();", button_login)
+        find_frame = await driver.find_element(By.TAG_NAME, 'iframe', timeout=15)
+        await asyncio.sleep(1)
+        iframe_document = await find_frame[0].content_document
+
+        checkbox = await iframe_document.find_element(By.CSS_SELECTOR, 'challenge-stage > div > label > input[type=checkbox] - checkbox', timeout=20)
+        await asyncio.sleep(10)
+        await driver.execute_script("arguments[0].click();", checkbox)
     except Exception as e:
-        print(f"BUTTON LOGIN \n{e}")
+        print(f"CLICK \n{e}")
 
     try:
         input_email = await driver.find_element(By.ID, 'el-2', timeout=15)
+        sleep(1.5)
         await input_email.clear()
         await input_email.write(user_login)
 
         input_password = await driver.find_element(By.ID, 'el-3', timeout=15)
+        sleep(1.5)
         await input_password.clear()
         await input_password.write(user_password)
     except Exception as e:
         print(f"INPUT DATA ERROR \n{e}")
 
     try:
-        login_button = await driver.find_element(By.CSS_SELECTOR, 'div > form > div:nth-child(8) > button')
+        login_button = await driver.find_element(By.CSS_SELECTOR,
+                                                 'body > div.wrapper > main > div > div.popup > form > div:nth-child(8) > button',
+                                                 timeout=10)
         await driver.execute_script("arguments[0].click();", login_button)
     except Exception as e:
         print(f"ERROR BUTTON LOGIN \n{e}")
@@ -64,8 +71,10 @@ async def login(driver):
 async def get_wallet():
     async with webdriver.Chrome(options=options) as driver:
         await login(driver)
+
         sleep(3)
         await driver.get('https://www.safeassets.com/deposit')
+
 
         try:
             choose_trc20 = await driver.find_element(By.CSS_SELECTOR, 'div.page__el.page__el_acc > div.payments-grid.payments__options.payments-custom > div > div:nth-child(11) > div > div > div.options > div:nth-child(1) > label', timeout=50)
@@ -102,4 +111,3 @@ def wallet():
     wallet_data = asyncio.run(get_wallet())
     print(wallet_data)
     return jsonify(wallet_data)
-
