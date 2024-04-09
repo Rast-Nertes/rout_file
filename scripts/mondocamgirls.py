e0d61023d96ba00045d5124d6c3f0a6ac362f047
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-04-09 10:41:52 +0300

    Update mondocamgirls.py

--- scripts/mondocamgirls.py
+++ scripts/mondocamgirls.py
@@ -2,10 +2,6 @@ from flask import jsonify
 from seleniumwire import webdriver
 from time import sleep
 from fake_useragent import UserAgent
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 # CONSTANS
