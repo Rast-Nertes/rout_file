2040248a5df497378ac09dc3f53427b7e92aef74
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-04-10 22:24:47 +0300

    Add files via upload

---
+++ scripts/cloudbet32.py
@@ -0,0 +1,130 @@
+from flask import jsonify
+from seleniumwire import webdriver
+from time import sleep
+from fake_useragent import UserAgent
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+# CONSTANS
+
+url = 'https://www.cloudbet32.com/ru/auth/sign-in'
+user_email = "fikeda5775@dacgu.com"
+user_password = "Qwerty62982"
+
+# CHROME CONSTANS
+
+options = webdriver.ChromeOptions()
+user_agent = UserAgent()
+options.add_argument(f"user-agent={user_agent.random}")
+options.add_argument("--disable-save-password-bubble")
+
+proxy_address = "196.19.121.187"
+proxy_login = 'WyS1nY'
+proxy_password = '8suHN9'
+proxy_port = 8000
+
+proxy_options = {
+    "proxy":{
+        "http":f"http://{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}",
+        "https": f"http://{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}"
+    }
+}
+
+#392ela921k91E
+
+
+def click(driver, time, XPATH):
+    driver.implicitly_wait(time)
+    elem_click = driver.find_element(By.XPATH, XPATH)
+    sleep(1.5)
+    driver.execute_script("arguments[0].click();", elem_click)
+
+
+def input_data(driver, time, XPATH, data):
+    driver.implicitly_wait(time)
+    elem_input = driver.find_element(By.XPATH, XPATH)
+    elem_input.clear()
+    elem_input.send_keys(data)
+
+
+def login(driver):
+    driver.get(url)
+    driver.maximize_window()
+
+    try:
+        input_data(driver, 35, '//*[@id="login"]', user_email)
+        sleep(1)
+        input_data(driver, 20, '//*[@id="password"]', user_password)
+    except Exception as e:
+        print(f'ERROR INPUT LOG DATA \n{e}')
+
+    try:
+        click(driver, 20, '//*[@id="root"]/div[1]/div/form/div[3]/div/button')
+    except Exception as e:
+        print(f'ERROR CLICK LOG BUT \n{e}')
+
+    try:
+        click(driver, 20, '//*[@id="root"]/div[1]/header/div/div/div[4]/button[3]')
+    except Exception as e:
+        print(f'ERROR DEPOS BUT \n{e}')
+    #
+    # try:
+    #     click(driver, 30, '//*[@id="Coin"]')
+    #     sleep(1)
+
+    input("Press")
+
+
+def get_wallet():
+    with webdriver.Chrome(options=options, seleniumwire_options=proxy_options) as driver:
+        login(driver)
+
+        try:
+            driver.implicitly_wait(60)
+            choose_trc20 = driver.find_element(By.ID, 'currency-15')
+            sleep(1.5)
+            driver.execute_script("arguments[0].click();", choose_trc20)
+
+            driver.implicitly_wait(40)
+            input_email = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/div[1]/div/form/div/div[2]/input')
+            input_email.clear()
+            input_email.send_keys(user_email)
+        except Exception as e:
+            print(f"INPUT EMAIL \n{e}")
+
+        try:
+            driver.implicitly_wait(60)
+            submit_payment = driver.find_element(By.ID, 'submit-payment')
+            sleep(1.5)
+            submit_payment.click()
+        except Exception as e:
+            print(f"SUBMIT ERROR \n{e}")
+
+        try:
+            sleep(3.5)
+            driver.implicitly_wait(60)
+            address = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/div[5]/div[1]/div[3]/div[7]/div[2]').text
+
+            driver.implicitly_wait(20)
+            amount = driver.find_element(By.XPATH, '/html/body/div[4]/div[2]/div/div[5]/div[1]/div[3]/div[5]/span').text
+
+            return {
+                "address": address,
+                "amount": amount,
+                "currency": "usdt"
+            }
+        except Exception as e:
+            print(f"DATA ERROR \n{e}")
+
+
+def wallet():
+    wallet_data = get_wallet()
+    print(wallet_data)
+    return jsonify(wallet_data)
+
+
+if __name__ == "__main__":
+    wallet()
