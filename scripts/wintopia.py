8a3a4df500a388f1dc60a0ef6ba7ed41ced6ec54
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-04-17 17:22:43 +0300

    Update wintopia.py

--- scripts/wintopia.py
+++ scripts/wintopia.py
@@ -105,17 +105,15 @@ async def get_wallet():
             copy_address = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/ul/li[2]/div/div[3]/a', timeout=60)
             await asyncio.sleep(2)
             await copy_address.click()
-
-            await asyncio.sleep(2)
+            await asyncio.sleep(3.5)
             address = pyperclip.paste()
 
-            await asyncio.sleep(3.5)
+            await asyncio.sleep(4.5)
 
             copy_amount = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/ul/li[1]/div/div[3]/a', timeout=20)
-            await asyncio.sleep(2.5)
-            await copy_amount.click()
-
             await asyncio.sleep(2)
+            await copy_amount.click()
+            await asyncio.sleep(3.5)
             amount = pyperclip.paste()
 
             return {
