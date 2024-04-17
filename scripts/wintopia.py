4532b81c4de56c26749937cbf82dbc6425d7b8d9
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-04-17 17:41:28 +0300

    Update wintopia.py

--- scripts/wintopia.py
+++ scripts/wintopia.py
@@ -7,9 +7,11 @@ from fake_useragent import UserAgent
 
 # CONSTANTS
 
-url = 'https://wintopia.co/de/login'
-user_email = "kiracase34@gmail.com"
-user_password = "kiramira123"
+url = 'https://winlegends.co/de/login'
+user_email = "kiracase34"
+user_password = "h@a8YA3LSgzmey"
+
+#213efi237l3
 
 # CHROME CONSTANTS
 
@@ -23,12 +25,6 @@ user_agent = UserAgent()
 options.add_argument(f"user-agent={user_agent.random}")
 options.add_argument("--disable-save-password-bubble")
 
-with open('config.txt') as file:
-    paths = file.readlines()
-    chrome_path = paths[0].strip()
-
-options.binary_location = chrome_path
-
 
 async def login(driver):
     await driver.maximize_window()
@@ -37,61 +33,58 @@ async def login(driver):
     await driver.get(url, timeout=60)
 
     try:
-        input_email = await driver.find_element(By.XPATH, '//*[@id="login"]', timeout=40)
+        input_email = await driver.find_element(By.XPATH, '//*[@id="login"]', timeout=20)
         await input_email.write(user_email)
 
-        input_pass = await driver.find_element(By.XPATH, '//*[@id="password"]', timeout=20)
-        await input_pass.write(user_password)
-    except Exception as e:
-        print(f'ERROR LOGIN DATA INPUT \n{e}')
+        input_password = await driver.find_element(By.XPATH, '//*[@id="password"]', timeout=20)
+        await input_password.write(user_password)
 
-    try:
-        log_but = await driver.find_element(By.XPATH, '//*[@id="popupSubmit"]', timeout=40)
+        login_button = await driver.find_element(By.XPATH, '//*[@id="popupSubmit"]', timeout=20)
         await asyncio.sleep(1)
-        await log_but.click()
+        await login_button.click()
     except Exception as e:
         print(f'ERROR LOGIN \n{e}')
 
-    await asyncio.sleep(2.5)
-    await driver.get('https://wintopia.co/de/account/personal-info', timeout=60)
+    await asyncio.sleep(3.5)
+    await driver.get('https://winlegends.co/de/wallet/deposit')
+    await asyncio.sleep(1.5)
+
+    await driver.execute_script("window.scrollBy(0, 400);")
+    await asyncio.sleep(1.5)
 
     try:
-        depos_but = await driver.find_element(By.XPATH, '//*[@id="__layout"]/div/div[1]/div[1]/main/div/div/div[1]/button', timeout=45)
-        await asyncio.sleep(3.5)
-        await depos_but.click()
-
-        choose_bit = await driver.find_element(By.XPATH, '//*[@id="cashierDepositPaymentList"]/div[3]/div/div[9]', timeout=20)
+        choose_bit = await driver.find_element(By.XPATH, '/html/body/div[1]/div/div/main/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div[2]/ul/li[13]/button', timeout=30)
         await asyncio.sleep(1)
-        await choose_bit.click()
+        await driver.execute_script("arguments[0].click();", choose_bit)
     except Exception as e:
-        print(f'ERROR CHOOSE BIT \n{e}')
+        print(f'ERROR CHOOSE TRC20 \n{e}')
 
     try:
-        depos = await driver.find_element(By.XPATH, '//*[@id="cashierDepositSubmit"]', timeout=40)
+        click_depos_but = await driver.find_element(By.XPATH, '//*[@id="walletDepositSubmit"]', timeout=20)
         await asyncio.sleep(1)
-        await depos.click()
+        await driver.execute_script("arguments[0].click();", click_depos_but)
     except Exception as e:
-        print(f'ERROR DEPOS \n{e}')
+        print(f'ERROR DEPOS BUT \n{e}')
 
     await asyncio.sleep(2)
     wind = await driver.window_handles
     await driver.switch_to.window(wind[0])
 
     try:
-        choose_tether = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/ul/li[6]/div[1]/div/span', timeout=80)
-        await asyncio.sleep(2.5)
+        choose_tether = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/ul/li[6]/div[1]/div/span', timeout=60)
+        await asyncio.sleep(1)
         await driver.execute_script("arguments[0].click();", choose_tether)
 
-        choose_trc20 = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/ul/li[6]/div[2]/ul/li[1]/a', timeout=60)
-        await asyncio.sleep(1.5)
+        choose_trc20 = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/ul/li[6]/div[2]/ul/li[1]/a', timeout=40)
+        await asyncio.sleep(1)
         await driver.execute_script("arguments[0].click();", choose_trc20)
     except Exception as e:
         print(f'ERROR CHOOSE TRC20 \n{e}')
 
     try:
-        pay_but = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/span[2]/button', timeout=80)
-        await asyncio.sleep(2.5)
-        await pay_but.click()
+        pay_but = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div/div[2]/div/span[2]/button', timeout=20)
+        await asyncio.sleep(1)
+        await driver.execute_script("arguments[0].click();", pay_but)
     except Exception as e:
         print(f'ERROR PAY BUT \n{e}')
 
@@ -100,21 +93,25 @@ async def get_wallet():
     async with webdriver.Chrome(options=options) as driver:
         await login(driver)
 
-        await asyncio.sleep(2.5)
+        await asyncio.sleep(4.5)
         try:
-            copy_address = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/ul/li[2]/div/div[3]/a', timeout=60)
-            await asyncio.sleep(2)
-            await copy_address.click()
-            await asyncio.sleep(3.5)
-            address = pyperclip.paste()
+            copy_amount = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/ul/li[1]/div/div[3]/a/span', timeout=20)
+            await asyncio.sleep(2.5)
+            await copy_amount.click()
 
+            await asyncio.sleep(1.5)
+            amount = pyperclip.paste()
+
+            await asyncio.sleep(1)
+            await driver.execute_script("window.scrollBy(0, 200);")
             await asyncio.sleep(4.5)
 
-            copy_amount = await driver.find_element(By.XPATH, '//*[@id="app"]/div/div/div[2]/div/div/ul/li[1]/div/div[3]/a', timeout=20)
-            await asyncio.sleep(2)
-            await copy_amount.click()
-            await asyncio.sleep(3.5)
-            amount = pyperclip.paste()
+            copy_address = await driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/div/div/ul/li[2]/div/div[3]/a', timeout=20)
+            await asyncio.sleep(2.5)
+            await copy_address.click()
+
+            await asyncio.sleep(1.5)
+            address = pyperclip.paste()
 
             return {
                 "address": address,
