38f7b56f4b1dd207845129c4bdfc8440d3393c8e
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-04-22 13:59:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import importlib.util
 
 app = Flask(__name__)
+
 
 def import_module(module_name):
     spec = importlib.util.find_spec(module_name)
@@ -9,13 +10,19 @@ def import_module(module_name):
     spec.loader.exec_module(module)
     return module
 
+
 @app.route('/api/selenium/<project_name>')
 def selenium_route(project_name):
     try:
         module = import_module(f'scripts.{project_name}')
         return module.wallet()
+
     except ModuleNotFoundError:
-        return 'Other project logic here'
+        return jsonify({"status": 0, "exception": f"Other project logic here."}), 500
+
+    except Exception as e:
+        return jsonify({"status": 0, "exception": f"{e}"}), 500
+
 
 if __name__ == '__main__':
     app.run(debug=True)
