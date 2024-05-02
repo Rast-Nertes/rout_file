e5e7e3a33c886f4731c16c6fdb68907223787489
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-05-02 14:52:27 +0300

    Update bspin.py

--- scripts/bspin.py
+++ scripts/bspin.py
@@ -1,5 +1,5 @@
 import asyncio
-import pyperclip
+import pyautogui
 from twocaptcha import TwoCaptcha
 from flask import jsonify
 from selenium_driverless import webdriver
@@ -8,7 +8,7 @@ from fake_useragent import UserAgent
 
 # CONSTANTS
 
-url = 'https://bspin.io/ru'
+url = 'https://bspin.io/ru/login'
 user_email = "kiracase34@gmail.com"
 user_password = "Kiramira123!"
 
@@ -31,7 +31,7 @@ with open('config.txt') as file:
     ext = paths[1].strip()
 
 options.binary_location = chrome_path
-# options.add_extension(ext)
+options.add_extension(ext)
 
 
 async def click(driver, time, XPATH):
@@ -52,12 +52,65 @@ async def login(driver):
     await asyncio.sleep(1)
     await driver.get(url, timeout=60)
 
+    handles = await driver.window_handles
+    print(handles)
+    for handle in handles:
+        await driver.switch_to.window(handle)
+        title = await driver.title
+        if "2Cap" in title:
+            break
+
     try:
-        await input_data(driver, 30, '//*[@id="desktop-header-container"]/div/div/div[2]/form/div[1]/div[2]/div[1]/input', user_email)
-        await input_data(driver, 30, '//*[@id="desktop-header-container"]/div/div/div[2]/form/div[1]/div[2]/div[2]/input', user_password)
-        await click(driver, 30, '//*[@id="desktop-header-container"]/div/div/div[2]/form/div[1]/div[2]/button')
+        await input_data(driver, 30, '//input[@name="apiKey"]', api_key)
+        await click(driver, 30, '//*[@id="connect"]')
+    except Exception as e:
+        print(f'ERROR CONNECT API \n{e}')
+
+    await asyncio.sleep(6.5)
+    pyautogui.press('enter')
+    await asyncio.sleep(2.5)
+
+    handles = await driver.window_handles
+    for handle in handles:
+        await driver.switch_to.window(handle)
+        title = await driver.title
+        if ("коин" in title) or ("bspin" in title):
+            if "bspin" in title:
+                await driver.refresh()
+                break
+
+    await asyncio.sleep(3.5)
+
+    try:
+        await input_data(driver, 30, '//*[@id="content"]/div/div[2]/div[2]/div/form/div[1]/input', user_email)
+        await input_data(driver, 30, '//*[@id="content"]/div/div[2]/div[2]/div/form/div[2]/input', user_password)
     except Exception as e:
         print(f'ERROR LOGIN \n{e}')
+
+    try:
+        await click(driver, 30, '//div[@class="captcha-solver captcha-solver_inner"]')
+    except Exception as e:
+        print(f'ERROR CLICK SOLVE CAPTCHA \n{e}')
+
+    while True:
+        try:
+            find_captcha_text = await driver.find_element(By.XPATH, '//div[@class="captcha-solver-info"]', timeout=10)
+            text = await find_captcha_text.text
+            if "шает" in text:
+                await asyncio.sleep(5)
+                print("Wait 5 seconds...")
+            else:
+                print("Solve")
+                break
+        except:
+            print("Solve")
+            break
+
+    await asyncio.sleep(2.5)
+    try:
+        await click(driver, 30, '//button[@class="xbtn large login-btn acc-btn login-page"]')
+    except Exception as e:
+        print(f'ERROR CLICK LOG BUT \n{e}')
 
     try:
         await click(driver, 30, '//*[@id="content"]/div[6]/div[1]/a')
@@ -99,4 +152,3 @@ def wallet():
     wallet_data = asyncio.run(get_wallet())
     print(wallet_data)
     return jsonify(wallet_data)
-
