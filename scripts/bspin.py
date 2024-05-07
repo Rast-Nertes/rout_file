98ccc4eb4888f89016a5e761bfd698a0c9c7471f
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-05-07 15:21:43 +0300

    Update bspin.py

--- scripts/bspin.py
+++ scripts/bspin.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from seleniumwire import webdriver
 from time import sleep
+import pyautogui
 from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -23,8 +24,9 @@ options.add_argument("--disable-save-password-bubble")
 with open('config.txt') as file:
     paths = file.readlines()
     chrome_path = paths[0].strip()
-    api_key = paths[3].strip()
-    ext = paths[1].strip()
+    api_anti = paths[2].strip()
+    api_key_solver = paths[5].strip()
+    ext = paths[4].strip()
 
 # options.binary_location = chrome_path
 options.add_extension(ext)
@@ -56,38 +58,43 @@ def input_data(driver, time, XPATH, data):
     elem_input.send_keys(data)
 
 
-def login(driver):
-    driver.get(url)
-    driver.maximize_window()
-    actions = ActionChains(driver)
+def api_connect():
+    sleep(0.4)
+    pyautogui.moveTo(1730, 75)
+    pyautogui.click()
 
-    handles = driver.window_handles
-    print(handles)
-    for handle in handles:
-        driver.switch_to.window(handle)
-        title = driver.title
-        if "2Cap" in title:
-            break
+    sleep(1)
 
-    try:
-        input_data(driver, 30, '//input[@name="apiKey"]', api_key)
-        click(driver, 30, '//*[@id="connect"]')
-    except Exception as e:
-        print(f'ERROR CONNECT API \n{e}')
-
-    WebDriverWait(driver, 20).until(EC.alert_is_present())
-    driver.switch_to.alert.accept()
+    for _ in range(2):
+        pyautogui.press('down')
+        sleep(0.15)
+    pyautogui.press('enter')
 
     sleep(1.5)
 
-    handles = driver.window_handles
-    for handle in handles:
-        driver.switch_to.window(handle)
-        title = driver.title
-        if ("коин" in title) or ("bspin" in title):
-            break
+    for _ in range(4):
+        pyautogui.press('tab')
+        sleep(0.15)
 
-    sleep(3.5)
+    sleep(2.5)
+    pyautogui.typewrite("CAP-3ED09723412F61BCD677E166EC9189AC", 0.075)
+    sleep(3)
+
+    pyautogui.moveTo(1540, 500)
+    # input("Press")
+    pyautogui.click()
+    sleep(2)
+    pyautogui.moveTo(1730, 75)
+    pyautogui.click()
+
+
+def login(driver):
+    driver.maximize_window()
+    api_connect()
+    sleep(2.5)
+    driver.get(url)
+    sleep(1.5)
+
     try:
         input_data(driver, 30, '//*[@id="content"]/div/div[2]/div[2]/div/form/div[1]/input', user_email)
         input_data(driver, 30, '//*[@id="content"]/div/div[2]/div[2]/div/form/div[2]/input', user_password)
@@ -95,44 +102,29 @@ def login(driver):
         print(f'ERROR LOGIN \n{e}')
 
     try:
-        click(driver, 30, '//div[@class="captcha-solver captcha-solver_inner"]')
-    except Exception as e:
-        print(f'ERROR CLICK SOLVE CAPTCHA \n{e}')
-
-    while True:
-        try:
+        while True:
             driver.implicitly_wait(10)
-            find_captcha_text = driver.find_element(By.XPATH, '//div[@class="captcha-solver-info"]')
-            text = find_captcha_text.text
-            if "шает" in text:
-                sleep(5)
-                print("Wait 5 seconds...")
-            else:
-                print("Solve")
+            find_check = driver.find_element(By.XPATH, '//*[@id="capsolver-solver-tip-button"]/div[2]').text
+            if "ена" in find_check:
+                click(driver, 30, '//*[@id="content"]/div/div[2]/div[2]/div/form/div[4]/button')
                 break
-        except:
-            print("Solve")
-            break
-
-    sleep(4.5)
+            else:
+                sleep(5)
+                print("Wait 5 seconds, captcha solving...")
+    except Exception as e:
+        print(f'ERROR CHECKBOX \n{e}')
 
     try:
-        click(driver, 30, '//button[@class="xbtn large login-btn acc-btn login-page"]')
+        sleep(3.5)
+        click(driver, 30, '//*[@id="content"]/div[6]/div[1]/a')
+        click(driver, 30, '//*[@id="user-action-buttons"]/a[2]')
     except Exception as e:
-        print(f'ERROR CLICK LOG BUT \n{e}')
         driver.implicitly_wait(20)
         find_input_tag = driver.find_element(By.XPATH, '//*[@id="content"]/div/div[2]/div[2]/div/form/div[1]/input')
         if find_input_tag:
             return {"status": "0", "ext": "Login error. Check script."}
         else:
             print(f"ERROR DEPOS BUT \n{e}")
-
-    try:
-        sleep(5.5)
-        click(driver, 30, '//*[@id="content"]/div[6]/div[1]/a')
-        click(driver, 30, '//*[@id="user-action-buttons"]/a[2]')
-    except Exception as e:
-        print(f'ERROR CLICK \n{e}')
 
     try:
         click(driver, 30, '//*[@id="desktop-header-container"]/div/div/div/div[2]/div')
