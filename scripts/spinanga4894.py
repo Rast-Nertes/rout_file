07b3356da8293e4cf4f9870f4ac5fbcf27795b6e
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-05-18 21:40:47 +0300

    Update spinanga4894.py

--- scripts/spinanga4894.py
+++ scripts/spinanga4894.py
@@ -5,36 +5,108 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 
-def wait_for_element(driver, timeout, by, value):
+# CONSTANS
+
+url = 'http://spinanga4894.com'
+user_email = "kiracase34@gmail.com"
+user_password = "Vbu7PsRg3a8K2Hf"
+
+
+# CHROME CONSTANS
+
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-save-password-bubble")
+options.add_argument("--auto-open-devtools-for-tabs")
+options.add_argument('--disable-popup-blocking')
+options.add_argument('--disable-notifications')
+options.add_argument('--log-level=3')
+options.add_argument('--disable-remote-fonts')
+
+prefs = {"profile.managed_default_content_settings.images": 2}
+options.add_experimental_option("prefs", prefs)
+
+with open('config.txt') as file:
+    paths = file.readlines()
+    chrome_path = paths[0].strip()
+    api_key = paths[3].strip()
+    ext = paths[1].strip()
+
+options.add_extension(ext)
+# options.binary_location = chrome_path
+
+proxy_address = "45.130.254.133"
+proxy_login = 'K0nENe'
+proxy_password = 'uw7RQ3'
+proxy_port = 8000
+#
+# proxy_address = "196.19.121.187"
+# proxy_login = 'WyS1nY'
+# proxy_password = '8suHN9'
+# proxy_port = 8000
+
+proxy_options = {
+    "proxy":{
+        "http":f"http://{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}",
+        "https": f"http://{proxy_login}:{proxy_password}@{proxy_address}:{proxy_port}"
+    }
+}
+
+
+def web_wait(driver, time):
+    return WebDriverWait(driver, time)
+
+
+def api_connect(driver):
+    sleep(1.5)
+    windows = driver.window_handles
+    for win in windows:
+        driver.switch_to.window(win)
+        sleep(1.5)
+        if "2Cap" in driver.title:
+            break
+
     try:
-        element = WebDriverWait(driver, timeout).until(
-            EC.presence_of_element_located((by, value))
-        )
-        return element
-    except TimeoutException:
-        print(f"Timeout: Element not found with {by} = {value}")
-        return None
+        js_click(driver, 30, '//*[@id="autoSolveRecaptchaV2"]')
+        js_click(driver, 30, '//*[@id="autoSolveInvisibleRecaptchaV2"]')
+        js_click(driver, 30, '//*[@id="autoSolveRecaptchaV3"]')
+        click(driver, 30, '//*[@id="config-form"]/div[2]/table/tbody/tr[4]/td[1]/div/div[1]')
+        click(driver, 30, '//*[@id="config-form"]/div[2]/table/tbody/tr[4]/td[1]/div/div[2]/div/div[2]')
+        input_data(driver, 30, '/html/body/div/div[1]/table/tbody/tr[1]/td[2]/input', api_key)
+        click(driver, 30, '//*[@id="connect"]')
+        sleep(4.5)
+        driver.switch_to.alert.accept()
+    except Exception as e:
+        print(f'ERROR CLICK \n{e}')
 
-def js_click(driver, timeout, XPATH):
-    element = wait_for_element(driver, timeout, By.XPATH, XPATH)
-    if element:
-        driver.execute_script("arguments[0].click();", element)
+    windows = driver.window_handles
+    for win in windows:
+        driver.switch_to.window(win)
+        sleep(1.5)
+        if not("2Cap" in driver.title):
+            break
 
-def click(driver, timeout, XPATH):
-    element = wait_for_element(driver, timeout, By.XPATH, XPATH)
-    if element:
-        element.click()
 
-def input_data(driver, timeout, XPATH, data):
-    element = wait_for_element(driver, timeout, By.XPATH, XPATH)
-    if element:
-        element.clear()
-        element.send_keys(data)
+def click(driver, time, XPATH):
+    driver.implicitly_wait(time)
+    elem_click = driver.find_element(By.XPATH, XPATH)
+    sleep(1.5)
+    elem_click.click()
+
+
+def js_click(driver, time, XPATH):
+    driver.implicitly_wait(time)
+    elem_click = driver.find_element(By.XPATH, XPATH)
+    sleep(1.5)
+    driver.execute_script("arguments[0].click();", elem_click)
+
+
+def input_data(driver, time, XPATH, data):
+    driver.implicitly_wait(time)
+    elem_input = driver.find_element(By.XPATH, XPATH)
+    elem_input.clear()
+    elem_input.send_keys(data)
+
 
 def login(driver):
     actions = ActionChains(driver)
@@ -43,7 +115,11 @@ def login(driver):
     driver.get(url)
 
     attempt = 0
-    while attempt <= 3:
+
+    while True:
+        if attempt > 3:
+            return {"error login. reload script"}
+
         try:
             click(driver, 60, '/html/body/stb-root/stb-base-layout/stb-header/header/div/button[1]/span')
             input_data(driver, 30, '//*[@id="email"]', user_email)
@@ -54,50 +130,44 @@ def login(driver):
 
         try:
             time_loop = 0
-            while time_loop <= 80:
-                find_check = wait_for_element(driver, 10, By.XPATH, '/html/body/div[1]/stb-overlay-container/div[2]/div/stb-login-dialog/stb-login-options/stb-stepper/div/stb-login-state-form/stb-login-steps/stb-stepper/stb-login-form/div/div[2]')
-                if find_check and ("ена" in find_check.text or "lve" in find_check.text):
+            while True:
+                driver.implicitly_wait(10)
+                find_check = driver.find_element(By.XPATH, '/html/body/div[1]/stb-overlay-container/div[2]/div/stb-login-dialog/stb-login-options/stb-stepper/div/stb-login-state-form/stb-login-steps/stb-stepper/stb-login-form/div/div[2]').text
+                if ("ена" in find_check) or ("lve" in find_check):
                     print("skip")
                     break
-                time_loop += 5
-                sleep(5)
-                print("Wait 5 seconds, captcha solving...")
+                else:
+                    if time_loop > 80:
+                        break
+                    time_loop += 5
+                    sleep(5)
+                    print("Wait 5 seconds, captcha solving...")
         except Exception as e:
-            print(f'ERROR CHECKBOX {e}')
+            print(f'ERROR CHECKBOX ')
 
         try:
-            find_error = wait_for_element(driver, 7.5, By.XPATH, '//p[@class="dialog-header__title"]')
-            if find_error and "ps" in find_error.text:
+            driver.implicitly_wait(7.5)
+            find_error = driver.find_element(By.XPATH, '//p[@class="dialog-header__title"]')
+            error = find_error.text
+            if "ps" in error:
+
                 click(driver, 10, '/html/body/div[1]/stb-overlay-container/div[2]/div/stb-request-failed-dialog/div/div/div[2]/button')
                 sleep(2.5)
                 attempt += 1
                 continue
             else:
-                print("Successful")
+                print("Succesfull")
                 break
-        except Exception as e:
-            print(f'ERROR AFTER LOGIN {e}')
+        except:
             break
 
     try:
         sleep(2.5)
-        js_click(driver, 70, '//button[@data-analytics-action="Balance"]')
+        js_click(driver, 30, '//button[@data-analytics-action="Balance"]')
     except Exception as e:
-        return {"status": "0", "ext": f"error depos but \n{e}"}
+        return {"status":"0", "ext":f"error depos but \n{e}"}
 
-    try:
-        sleep(4.5)
-        shadow_host = wait_for_element(driver, 30, By.CSS_SELECTOR, 'div[id="widget"]')
-        shadow_root = shadow_host.shadow_root if shadow_host else None
 
-        sleep(10.5)
-        shadow_root.find_element(By.CSS_SELECTOR, 'button[data-testid="expand-list-button"]').click()
-
-        sleep(2.5)
-        find_trc20_image = shadow_root.find_element(By.CSS_SELECTOR, 'img[alt="USDTTRONTRC20"]')
-        driver.execute_script("arguments[0].click();", find_trc20_image)
-    except Exception as e:
-        return {"status": "0", "ext": f"error shadow \n{e}"}
 
 def get_wallet():
     with webdriver.Chrome(options=options, seleniumwire_options=proxy_options) as driver:
@@ -105,21 +175,38 @@ def get_wallet():
         if log:
             return log
 
-        try:
-            sleep(4.5)
-            shadow_root = wait_for_element(driver, 10, By.CSS_SELECTOR, 'div[id="widget"]').shadow_root
+        sleep(12.5)
+        shadow = driver.find_element(By.CSS_SELECTOR, 'div[id="widget"]').shadow_root
 
-            amount = shadow_root.find_element(By.CSS_SELECTOR, '#react-root-container > div > div:nth-child(4) > div > div > div > input').get_attribute('min')
+        try:
+            sleep(6.5)
+            shadow.find_element(By.CSS_SELECTOR, 'button[data-testid="expand-list-button"]').click()
+
+            sleep(2.5)
+            img_trc20 = shadow.find_element(By.CSS_SELECTOR, 'img[alt="USDTTRONTRC20"]')
+            driver.execute_script("arguments[0].click();", img_trc20)
+        except Exception as e:
+            return {"status": "0", "ext": f"error shadow \n{e}"}
+
+        try:
+            sleep(5)
+            amount_elem = shadow.find_element(By.CSS_SELECTOR, '#react-root-container > div > div:nth-child(4) > div > div > div > input')
+            amount = amount_elem.get_attribute('min')
             print(amount)
 
-            sleep(1.5)
-            shadow_root.find_element(By.CSS_SELECTOR, '#react-root-container > div > div:nth-child(4) > div > div > button').click()
+            sleep(2.5)
+            depos_but = shadow.find_element(By.CSS_SELECTOR, '#react-root-container > div > div:nth-child(4) > div > div > button')
+            depos_but.click()
+            
+            sleep(3)
+            get_src = shadow.find_element(By.CSS_SELECTOR, '#react-root-container > div > div:nth-child(4) > div > iframe')
+            src = get_src.get_attribute('src')
 
-            get_src = shadow_root.find_element(By.CSS_SELECTOR, '#react-root-container > div > div:nth-child(4) > div > iframe').get_attribute('src')
-            driver.get(get_src)
+            driver.get(src)
 
-            address_elem = wait_for_element(driver, 30, By.XPATH, '//div[@class="wallet-address"]')
-            address = address_elem.text if address_elem else "N/A"
+            driver.implicitly_wait(30)
+            address_elem = driver.find_element(By.XPATH, '//div[@class="wallet-address"]')
+            address = address_elem.text
 
             return {
                 "address": address,
@@ -127,7 +214,9 @@ def get_wallet():
                 "currency": "usdt"
             }
         except Exception as e:
-            return {"status": "0", "ext": f"error data \n{e}"}
+            input("press 2")
+            return {"status":"0", "ext":f"error data \n{e}"}
+
 
 def wallet():
     wallet_data = get_wallet()
