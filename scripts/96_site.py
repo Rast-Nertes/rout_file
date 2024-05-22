f3fef9e0cfc0d79157734fb566f82edf93771f97
Rast-Nertes <152014428+Rast-Nertes@users.noreply.github.com>
2024-05-22 10:08:31 +0300

    Update 96_site.py

--- scripts/96_site.py
+++ scripts/96_site.py
@@ -69,7 +69,7 @@ def login(driver):
     driver.get(url)
 
     try:
-        js_click(driver, 30, '/html/body/div[1]/div[1]/div[1]/header/div/div/div[2]/div[1]/span')
+        js_click(driver, 90, '/html/body/div[1]/div[1]/div[1]/header/div/div/div[2]/div[1]/span')
         input_data(driver, 30, '/html/body/div[1]/div[2]/div/div/div/div[4]/div/input', user_email)
         input_data(driver, 30, '//*[@id="app"]/div[2]/div/div/div/div[5]/div/input', user_password)
         click(driver, 30, '//*[@id="app"]/div[2]/div/div/div/button/span')
