5f1e923d0916b028c12b3575fddfcd86a8a91892
Eldar <rast.nertes@gmail.com>
2024-06-03 17:36:51 +0300

    update script

--- scripts/crdstore.py
+++ scripts/crdstore.py
@@ -32,40 +32,36 @@ async def login(driver):
         choose_country = await driver.find_element(By.ID, 'wd-add-to-cart', timeout=50)
         await choose_country.click()
     except Exception as e:
-        print(f"ERROR CHOOSE \n{e}")
+        return {"status":"0", "ext":f"error choose {e}"}
 
     try:
+        await asyncio.sleep(5)
         input_first_name = await driver.find_element(By.ID, 'billing_first_name', timeout=30)
         await input_first_name.write("Kira")
 
         input_last_name = await driver.find_element(By.ID, 'billing_email', timeout=30)
         await input_last_name.write(user_login)
 
-        sleep(1.5)
+        await asyncio.sleep(3)
         terms_click = await driver.find_element(By.ID, 'terms', timeout=30)
         await terms_click.click()
     except Exception as e:
-        print(f"ERROR INPUT DATA \n{e}")
+        return {"status":"0", "ext":f"error input data {e}"}
 
     try:
         choose_wallet = await driver.find_element(By.ID, 'mcc_currency_id', timeout=30)
-        await asyncio.sleep(3.5)
-        await choose_wallet.click()
-
-        for _ in range(6):
-            pyautogui.press('down')
-            sleep(0.5)
-        pyautogui.press('enter')
-        sleep(0.5)
+        await asyncio.sleep(3)
+        await driver.execute_script("arguments[0].value = 'USDT_TRON';", choose_wallet)
+        await asyncio.sleep(1.5)
     except Exception as e:
-        print(f"ERROR CHOOSE \n{e}")
+        return {"status":"0", "ext":f"error choose trc20{e}"}
 
     try:
         sleep(1.5)
         place_order = await driver.find_element(By.ID, 'place_order', timeout=20)
         await place_order.click()
     except Exception as e:
-        print(f"ERROR TERMS \n{e}")
+        return {"status":"0", "ext":f"error terms {e}"}
 
 
 async def get_wallet():
@@ -74,14 +70,13 @@ async def get_wallet():
 
         try:
             amount_element = await driver.find_element(By.XPATH, '//*[@id="post-10"]/div/div/div/div[3]/div/p/span[1]/span/input', timeout=30)
-            amount_attribute = await amount_element.__getattribute__('value')
+            amount_attribute = await amount_element.get_attribute('value')
             amount = str(amount_attribute).replace("USDT_TRON", "").replace(" ", '')
 
             address_element = await driver.find_element(By.XPATH, '//*[@id="post-10"]/div/div/div/div[3]/div/p/span[2]/span/input', timeout=30)
-            address = await address_element.__getattribute__('value')
+            address = await address_element.get_attribute('value')
         except Exception as e:
-            print(f"DATA ERROR \n{e}")
-            return "Not work data"
+            return {"status": "0", "ext": f"error data {e}"}
 
         return {
             "address": address,
