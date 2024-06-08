b130a50b44660d76a998954f5053bcfde2ef1356
Eldar <rast.nertes@gmail.com>
2024-06-08 23:36:47 +0300

    update script

--- scripts/luckyblock.py
+++ scripts/luckyblock.py
@@ -12,7 +12,7 @@ user_email = "kiracase34@gmail.com"
 user_password = "kiramira123"
 
 # CHROME CONSTANS
-#C:/Users/Acer/Desktop/py_scripts/result/ROUT_FILE/config.txt
+
 with open('config.txt') as file:
     paths = file.readlines()
     chrome_path = paths[0].strip()
@@ -37,41 +37,29 @@ async def login(driver):
         await asyncio.sleep(0.5)
         await input_pass.write(user_password)
     except Exception as e:
-        print(f'ERROR INPUT DATA \n{e}')
+        return {"status":"0", "ext":f"error input data {e}"}
 
     try:
         log_but = await driver.find_element(By.ID, 'login-form-submit-button', timeout=20)
         await asyncio.sleep(1.5)
         await log_but.click()
     except Exception as e:
-        print(f"ERROR LOG BUT \n{e}")
-
-    try:
-        data_but = await driver.find_element(By.XPATH,
-                                             '//*[@id="root"]/div[6]/div[1]/div[2]/header/div/div/div[1]/div/button',
-                                             timeout=20)
-        await asyncio.sleep(1.5)
-        await data_but.click()
-
-        data_but = await driver.find_element(By.XPATH,
-                                             '//*[@id="root"]/div[6]/div[1]/div[2]/header/div/div/div[1]/div/button',
-                                             timeout=20)
-        await asyncio.sleep(1.5)
-        await data_but.click()
-    except Exception as e:
-        print(f'ERROR LOG BUT\n{e}')
+        return {"status":"0", "ext":f"error log but {e}"}
 
 
 async def get_wallet():
     async with webdriver.Chrome(options=options) as driver:
-        await login(driver)
+        log = await login(driver)
+        if log:
+            return log
+
 
         try:
             await asyncio.sleep(4.5)
             address_elem = await driver.find_element(By.ID, 'deposit-address', timeout=30)
             address = await address_elem.text
 
-            amount_elem = await driver.find_element(By.XPATH, '/html/body/div[8]/div[3]/div/div/div/div/div[2]/div/p', timeout=30)
+            amount_elem = await driver.find_element(By.XPATH, '(//p[@class])[3]', timeout=30)
             amount = await amount_elem.text
         except Exception as e:
             print(f'ERROR DATA \n{e}')
