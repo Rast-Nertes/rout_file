30384ae1b764f0e936aaf758508b50b115c325aa
Eldar <rast.nertes@gmail.com>
2024-06-19 12:07:37 +0300

    update script

--- scripts/fakeidndl.py
+++ scripts/fakeidndl.py
@@ -112,7 +112,7 @@ def get_wallet():
                 "currency": "usdt"
             }
         except Exception as e:
-            print(f"ERROR DATA \n{e}")
+            return {"status":"0", "ext":f"error data {e}"}
 
 
 def wallet():
