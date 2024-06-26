09bf4c02532eb7a1512378a78cf45837899a8571
Eldar <rast.nertes@gmail.com>
2024-06-26 22:49:24 +0300

    update script

--- scripts/bilshara.py
+++ scripts/bilshara.py
@@ -142,7 +142,7 @@ def get_wallet():
 
             return {
                 "address": address,
-                "amount": amount,
+                "amount": amount.replace("USDT", ''),
                 "currency": "usdt"
             }
 
